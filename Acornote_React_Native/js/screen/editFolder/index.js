babe8b783f3bec6dab22eb59ee91f2cac1eb56c7
Tonny <tonny.litao@gmail.com>
2017-02-16 12:23:58 +1300

    fix the height bug of textinput in edit folder

--- Acornote_React_Native/js/screen/editFolder/index.js
+++ Acornote_React_Native/js/screen/editFolder/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { StyleSheet, Modal, KeyboardAvoidingView, TextInput, View, } from 'react-native';
+import { StyleSheet, Modal, KeyboardAvoidingView, TextInput, View, StatusBar, } from 'react-native';
 
 import IconButton from 'Acornote_React_Native/js/components/iconButton'
 
@@ -33,10 +33,16 @@ export default class EditFolderScreen extends Component {
           animationType={"slide"}
           transparent={false}
           visible={this.props.modalVisible}
+          onRequestClose={()=>{}}
           >
-          <KeyboardAvoidingView behavior='height' style={styles.view}>
+          <StatusBar 
+            hidden={true}
+            animated={true} 
+            showHideTransition="slide"
+          />
+          <KeyboardAvoidingView behavior='height' style={styles.view} >
          
-            <View style={styles.buttons}>
+            <View style={styles.canselSureBtns}>
               <IconButton 
                 onPress={this.props.onCancel} 
                 source={require('Acornote_React_Native/img/ic_cancel@3x.png')} />
@@ -52,10 +58,15 @@ export default class EditFolderScreen extends Component {
               style={styles.textInput}
               placeholder="Folder name"
               autoFocus={true}
+              autoCorrect={true}
               onChangeText={(text) => this.setState({text})}
+              clearButtonMode="while-editing"
+              returnKeyType="done"
+              enablesReturnKeyAutomatically={true}
+              underlineColorAndroid="transparent"
             />
 
-            <View style={styles.buttons}>
+            <View style={styles.bottomBtns}>
               <IconButton 
                 onPress={this._onEditLink} 
                 source={require('Acornote_React_Native/img/ic_link_off@3x.png')} />
@@ -82,21 +93,29 @@ export default class EditFolderScreen extends Component {
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    backgroundColor: 'red',
+    flexDirection:'column',
+    backgroundColor: '#19D2B9',
     justifyContent: 'space-between',
     alignItems: 'stretch',
   },
   textInput: {
+    flex: 1,
     color: 'white',
     textAlign: 'center',
     fontSize: 50,
     marginRight: 10,
     marginLeft: 10,
   },
-  buttons: {
+  canselSureBtns: {
     margin: 10,
     height: 44,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  bottomBtns: {
+    margin: 10,
+    height: 44,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 })
